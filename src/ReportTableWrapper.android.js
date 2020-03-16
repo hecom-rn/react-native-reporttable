5debbd0334678d822d49bd30589e761beadb913b
hellomjx <2312951425@gmail.com>
2020-03-16 16:17:35 +0800

    修改安卓的ReportTableWrapper

--- src/ReportTableWrapper.android.js
+++ src/ReportTableWrapper.android.js
@@ -11,14 +11,15 @@ export default class ReportTableWrapper extends React.Component{
             scrollY: new Animated.Value(0),
             currentFreezeRow: false,
         };
-        this.headerHeight = 100;
+        this.headerHeight = this.props.headerView() && this.props.headerView().props.style
+            && this.props.headerView().props.style.height;
     }
 
 
     render() {
-        const { data } = this.props;
+        console.log(this.props)
+        const { data, size } = this.props;
         const dataStr = JSON.stringify(data);
-        console.log(dataStr)
         return  <Animated.ScrollView
             ref={v => this.outVScroll = v}
             automaticallyAdjustContentInsets={false}
@@ -28,15 +29,14 @@ export default class ReportTableWrapper extends React.Component{
                 useNativeDriver: true,
                 listener: this._onVerticalScroll
             })}
+            style={{flex: 1}}
         >
-
             {this.props.headerView && this.props.headerView()}
             <ReportTableView
                 ref={v => this.reportTable = v}
-                style={{width: 300, height: 600 }}
-                {...this.props}
-                data={dataStr}
                 freezeRowFun={this.state.freezeRow}
+                data={dataStr}
+                style={[size]}
             />
 
         </Animated.ScrollView>
@@ -44,9 +44,6 @@ export default class ReportTableWrapper extends React.Component{
 
 
     _onVerticalScroll = (event) => {
-        console.log(event.nativeEvent.contentOffset)
-        //currentFreezeRow是false的时候，大于50冻结
-        //true的时候，小于50解冻
         const {currentFreezeRow} = this.state;
         if(currentFreezeRow && event.nativeEvent.contentOffset.y <  this.headerHeight){
             this.setState({currentFreezeRow: !currentFreezeRow})
