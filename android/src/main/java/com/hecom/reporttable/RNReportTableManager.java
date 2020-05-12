457b07a6488235664ec2f0bc54360d8fa2201d97
hellomjx <2312951425@gmail.com>
2020-05-12 17:14:39 +0800

    表格的高度和宽度的double类型处理

--- android/src/main/java/com/hecom/reporttable/RNReportTableManager.java
+++ android/src/main/java/com/hecom/reporttable/RNReportTableManager.java
@@ -43,37 +43,49 @@ public class RNReportTableManager extends SimpleViewManager<View> {
 
     @ReactProp(name = "data")
     public void setData(View view, String dataSource) {
-        if(reportTableConfig == null){
-            return;
-        }
+         if(reportTableConfig == null){
+                    return;
+                }
 
-        String jsonData = "";
-        int minHeight = 40;
-        int minWidth = 50;
-        int maxWidth = 120;
+                String jsonData = "";
+                int minHeight = 40;
+                int minWidth = 50;
+                int maxWidth = 120;
 
-        try {
-            JSONObject object = new JSONObject(dataSource);
-            if(object.has("data")){
-                Object dataObj = object.get("data");
-                jsonData = dataObj.toString();
-            }
-            if(object.has("minHeight")){
-                minHeight = (int) object.get("minHeight");
-            }
-            if(object.has("minWidth")){
-                minWidth = (int) object.get("minWidth");
-            }
-            if(object.has("maxWidth")){
-                maxWidth = (int) object.get("maxWidth");
-            }
-            TableConfigBean configBean = new TableConfigBean(minWidth, maxWidth, minHeight);
-            reportTableConfig.setReportTableData(view, jsonData, configBean);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+                try {
+                    JSONObject object = new JSONObject(dataSource);
+                    if(object.has("data")){
+                        Object dataObj = object.get("data");
+                        jsonData = dataObj.toString();
+                    }
+                    if(object.has("minHeight")){
+                        minHeight = transformDataType(object.get("minHeight"));
+                    }
+                    if(object.has("minWidth")){
+                        minWidth = transformDataType(object.get("minWidth")); ;
+
+                    }
+                    if(object.has("maxWidth")){
+                        maxWidth = transformDataType(object.get("maxWidth"));;
+                    }
+                    TableConfigBean configBean = new TableConfigBean(minWidth, maxWidth, minHeight);
+                    reportTableConfig.setReportTableData(view, jsonData, configBean);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
     }
 
+    private int transformDataType(Object data){
+            int result = 0;
+            if(data instanceof Double){
+                result = (int)Math.round(((Double)data));
+            }else if(data instanceof Float){
+                result = (int)Math.round(((Float)data));
+            }else if(data instanceof Integer){
+                result = (int)data;
+            }
+            return result;
+        }
 
     private void setConfig(String dataSource){
         String jsonData = "";
