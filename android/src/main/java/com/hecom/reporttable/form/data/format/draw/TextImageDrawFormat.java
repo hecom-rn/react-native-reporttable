7f4cc5165eb92df984075751dc6c5daa6d404f7c
gaopeilei <gaopeilei@hecom.cn>
2023-02-16 11:18:08 +0800

    fix(SmartTable): 更正表格图标绘制边界
    --bug=1035567 --user=高培磊
    【测试】【app】【审批流4.1】审批流编辑页，子明细列表样式操作列未显示删除图标，但点击可删除数据（vivo）
    https://www.tapd.cn/23787791/s/1643584

--- android/src/main/java/com/hecom/reporttable/form/data/format/draw/TextImageDrawFormat.java
+++ android/src/main/java/com/hecom/reporttable/form/data/format/draw/TextImageDrawFormat.java
@@ -114,7 +114,7 @@ public abstract class TextImageDrawFormat<T> extends ImageResDrawFormat<T> {
                 this.rect.set(rect.left,rect.top ,rect.right-(imgWidth+drawPadding),rect.bottom);
                 textDrawFormat.drawImageText(c,this.rect,cellInfo,config);
                 //int imgLeft = (rect.right+rect.left)/2+ textDrawFormat.measureWidth(cellInfo.column,cellInfo.row,config)/2 + drawPadding;
-                int imgLeft = rect.right - 35;
+                int imgLeft = rect.right - imgWidth;
                 this.rect.set(imgLeft,rect.top,imgLeft+imgWidth,rect.bottom);
                 super.draw(c,this.rect,cellInfo,config);
                 break;
