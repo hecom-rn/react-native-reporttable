d6e32c03acb9d88ab1820735776545617cc881b9
Ma Shuai <mashuai_hy@163.com>
2023-03-10 09:12:52 +0800

    fix: 撤回iOS测试时分割线的颜色

--- ios/ReportTableView.m
+++ ios/ReportTableView.m
@@ -71,7 +71,7 @@
     CGFloat hairline = 1;
     
     self.spreadsheetView.intercellSpacing = CGSizeMake(hairline, hairline);
-    self.spreadsheetView.gridStyle = [[GridStyle alloc] initWithStyle:GridStyle_solid width: hairline color: [UIColor redColor]];
+    self.spreadsheetView.gridStyle = [[GridStyle alloc] initWithStyle:GridStyle_solid width: hairline color: reportTableModel.lineColor];
     
     [self.spreadsheetView reloadData];
     [self scrollViewDidZoom: self];
