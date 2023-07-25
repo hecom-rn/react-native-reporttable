21bf936434e309bf2f17bb20fb325fd919611935
gaopeilei <gaopeilei@hecom.cn>
2023-07-25 10:04:27 +0800

    chore(UI): clean

--- src/ReportTableWrapper.android.js
+++ src/ReportTableWrapper.android.js
@@ -108,10 +108,6 @@ export default class ReportTableWrapper extends React.Component {
         return ReactNative.findNodeHandle(this.refs.AndroidReportTableView);
     };
 
-    sscrollToBottom = () => {
-        this.table.scrollToBottom();
-    }
-
     _toAndroidData = (props, headerHeight) => {
         // let {headerHeight} = this.state;
         const {
