5ce50b93d0ad4794270f967ff5f3ff295576531d
白宇东 <summer88123@163.com>
2024-08-27 14:42:59 +0800

    feat: 鸿蒙初始化
    暂时提供空接口

---
+++ src/ReportTableWrapper.harmony.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { AppRegistry, View, Text } from 'react-native';
+
+export default class ReportTableWrapper extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.headerViewSize = { width: 0, height: 0 };
+        this.handleData(props);
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        this.handleData(nextProps);
+    }
+
+    handleData = (props) => {
+        const defaultHeader = () => <View />;
+        AppRegistry.registerComponent('ReportTableHeaderView', () => props.headerView || defaultHeader);
+        if (props.headerView && props.headerView()) {
+            const {width, height} = props.headerView().props.style;
+            this.headerViewSize = {height, width};
+        } else {
+            this.headerViewSize = {width: 0, height:0};
+        }
+    }
+
+    scrollTo = (params) => {
+        const { lineX = 0, lineY = 0, offsetX = 0, offsetY = 0, animated = true } = params || {};
+        this.table.scrollTo([lineX, lineY, offsetX, offsetY, animated]);
+    }
+
+    spliceData = (params) => {
+        this.table.spliceData([params]);
+    }
+
+    updateData = (params) => {
+        this.table.updateData([params.data, params.y, params.x]);
+    }
+
+    scrollToBottom = () => {
+        this.table.scrollToBottom();
+    }
+
+    onClickEvent = ({nativeEvent: {keyIndex, rowIndex, columnIndex, verticalCount, horizontalCount}}) => {
+        this.props.onClickEvent && this.props.onClickEvent({keyIndex, rowIndex, columnIndex, verticalCount, horizontalCount});
+    };
+
+    render() {
+        return (
+            <View><Text>测试组件，暂无实现</Text></View>
+        );
+    }
+}
